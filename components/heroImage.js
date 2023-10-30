ed52d58a4bbd08a2c40b779b01459331d988d7d7
hayashibara <yujinyuto1223@gmail.com>
2023-10-30 14:36:03 +0900

    add HeroImageAnimation but 'undefined bug' occur

--- components/heroImage.js
+++ components/heroImage.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { Box, Text, useBreakpointValue } from "@chakra-ui/react";
 
 const HeroImage = () => {
@@ -8,12 +9,44 @@ const HeroImage = () => {
 
     return 'My mission is to pursue and constantly improve  \${combine(elements)}';
     makeWorldBetter();
-};
+    };
   `;
+  const shortText = "Ahoxa();";
 
-  const shortText = `Ahoxa();`;
+  const breakpointValue = useBreakpointValue({ base: shortText, md: fulltext });
+  const [targetText, setTargetText] = useState(breakpointValue);
+  const [isFirstRender, setIsFirstRender] = useState(true);
+  const [displayText, setDisplayText] = useState("");
 
-  const DisplayText = useBreakpointValue({ base: shortText, md: fulltext });
+  useEffect(() => {
+    if (isFirstRender) {
+      setIsFirstRender(false);
+      return;
+    }
+  }, []);
+
+  useEffect(() => {
+    setTargetText(breakpointValue);
+    setDisplayText("");
+  }, [breakpointValue]);
+
+  useEffect(() => {
+    let index = 0;
+
+    const typeInterval = setInterval(() => {
+      if (index < targetText.length) {
+        setDisplayText((prevText) => prevText + targetText[index]);
+        index++;
+      } else {
+        if (displayText.endsWith("undefined")) {
+          setDisplayText((prevText) => prevText.replace("undefined", ""));
+        }
+        clearInterval(typeInterval);
+      }
+    }, 50);
+
+    return () => clearInterval(typeInterval);
+  }, [targetText]);
 
   return (
     <Box
@@ -28,7 +61,7 @@ const HeroImage = () => {
       zIndex={1}
     >
       <Text color="white" fontSize="2rem">
-        {DisplayText}
+        {displayText}
       </Text>
     </Box>
   );
