99279522712de453df250831ac252b71b71d3c09
hayashibara <yujinyuto1223@gmail.com>
2023-11-01 13:44:38 +0900

    add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -7,12 +7,48 @@ export default class Document extends NextDocument {
     return (
       <Html lang="en">
         <Head>
+          {/* Font settings */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@300&family=Inika:wght@700&family=Noticia+Text&display=swap"
             rel="stylesheet"
           />
+
+          {/* Favicon settings */}
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicon/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/favicon/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/favicon/safari-pinned-tab.svg"
+            color="#5bbad5"
+          />
+          <meta name="msapplication-TileColor" content="#2b5797" />
+          <meta
+            name="msapplication-config"
+            content="/favicon/browserconfig.xml"
+          />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
