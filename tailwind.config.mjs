4d6b2e9ca71e7d51e9fb2ff9a2c6a42756c2d11a
hayashibara <yujinyuto1223@gmail.com>
2023-12-28 15:19:05 +0900

    add color setting

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,16 @@ export default {
         noticia: ["Noticia Text", "serif"],
         cardo: ["Cardo", "serif"],
       },
+      colors: {
+        beige: {
+          100: "#EEE2B6",
+          200: "#D4CCAC",
+        },
+        navy: {
+          100: "#295C82",
+          200: "#1D2B58",
+        },
+      },
     },
   },
   plugins: [require("daisyui")],
